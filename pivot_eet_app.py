d7c07c148208eaa1d8321148fe7312d5a4f63fdf
QuentinCarriou <quentin.carriou@wee-fin.com>
2026-03-25 18:27:49 +0100

    add subtitle + output file name

--- pivot_eet_app.py
+++ pivot_eet_app.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import streamlit as st
 from io import BytesIO
+from pathlib import Path
 
-st.title("WeeFin - Outil de transformation des templates de collecte EET")
+st.title("WeeFin")
+st.subheader("Outil de transformation des templates de collecte EET")
 
 uploaded_file = st.file_uploader("Uploadez votre template de collecte EET WeeFin", type="xlsx")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
     result = pd.concat([df[["Datapoint Name"]], df.iloc[:, 10:]], axis=1).set_index("Datapoint Name").T
+    output_name = Path(uploaded_file.name).stem + "_pivoted.xlsx"
     
     # Convert to Excel in memory
     output = BytesIO()
@@ -19,6 +22,6 @@ if uploaded_file:
     st.download_button(
         label="Télécharger le fichier au format EET",
         data=output,
-        file_name="output_pivoted.xlsx",
+        file_name=output_name,
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
